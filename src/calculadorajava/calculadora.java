d46830ae6cd3a7575ebe6113f8e402cccff76d60
joissiairane <joissi.airane@gmail.com>
2023-03-16 20:27:26 -0300

    1alteracao

---
+++ src/calculadorajava/calculadora.java
@@ -0,0 +1,17 @@
+package calculadorajava;
+
+public class calculadora {
+	public double Adicao(double a, double b) {
+		double resultado = a + b;
+		return resultado;
+	}
+	public double Subtracao(double a, double b) {
+		return a - b;
+	}
+	public double Divisao(double a, double b) {
+		return a / b;
+	}
+	public double Multiplicacao(double a, double b) {
+		return a * b;
+	}
+}
---
+++ src/calculadorajava/main.java
@@ -0,0 +1,15 @@
+package calculadorajava;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		calculadora calc = new calculadora();
+		
+		System.out.println(calc.Adicao(10, 10));
+		System.out.println(calc.Subtracao(10, 10));
+		System.out.println(calc.Divisao(10, 10));
+		System.out.println(calc.Multiplicacao(10, 10));
+	}
+
+}
